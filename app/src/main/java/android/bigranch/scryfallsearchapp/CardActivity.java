4914e69acff3c4bf4a62089ff7cee9a56ee976d8
GeorgeRJacobs <george.richard.jacobs@gmail.com>
2020-03-28 11:33:19 -0600

    added basic card activity

---
+++ app/src/main/java/android/bigranch/scryfallsearchapp/CardActivity.java
@@ -0,0 +1,39 @@
+package android.bigranch.scryfallsearchapp;
+
+import android.bigranch.scryfallsearchapp.models.Card;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class CardActivity extends BaseActivity {
+
+    //UI Components
+    private AppCompatImageView mCardImage;
+    private TextView mCardTitle, mCardType, mCardText, mCardPrice;
+    private LinearLayout mCardDetailsContainer;
+    private ScrollView mScrollview;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+        mCardImage = findViewById(R.id.card_image_act);
+        mCardTitle = findViewById(R.id.card_title_act);
+        mCardType = findViewById(R.id.card_type_act);
+        mCardText = findViewById(R.id.card_text_act);
+        mCardPrice = findViewById(R.id.card_price_act);
+        mScrollview = findViewById(R.id.parent);
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent() {
+        if(getIntent().hasExtra("card")){
+            Card card = getIntent().getParcelableExtra("card");
+        }
+    }
+}
